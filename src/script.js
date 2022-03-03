a8148345004173dc9aae70fe32a7ad6cae5f8ad9
rajat <rkumar201@sapient.com>
2022-03-02 19:53:52 -0600

    adde npm and webpack

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
+import './style.css'
+import * as THREE from "three";
+
 
 
 // Scene
@@ -41,7 +44,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 
 // first render
-renderer.render(scene, camera) 
+renderer.render(scene, camera) ;
 
 // the position of the camera is in x, y, z
 // z is forward-backward axis in 3JS
